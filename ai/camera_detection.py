68cdb3612e2078139594948b7746e1736a275347
Chase Galloway <chase.h.galloway21@gmail.com>
2023-09-07 09:07:59 -0400

    Added a few lines of code for bug fixes

--- ai/camera_detection.py
+++ ai/camera_detection.py
@@ -6,8 +6,12 @@ classifier_path = "training_data/classifier.xml"
 # Load the trained classifier for detection
 svm = cv2.ml.SVM_load(classifier_path)
 
+print("Classifier loaded")
+
 # Create the Haar cascade classifier for detection
 detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+print("Haar cascade classifier created")
 
 # Initialize the video capture
 cap = cv2.VideoCapture(0)
@@ -30,24 +34,24 @@ while running:
 
     # Draw bounding boxes around the detected hickory sticks
     for (x, y, w, h) in sticks:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.putText(frame, 'stick', (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 0.9, (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (175, 63, 160), 2)
+        cv2.putText(frame, 'stick', (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 0.9, (175, 63, 160), 2)
 
     # Display the processed frame on a screen
-    cv2.imshow('Frame', frame)
+    cv2.imshow('v0.2 Incomplete', frame)
 
     # Check for key press
     key = cv2.waitKey(1) & 0xFF
 
-    # Key actions
+    # Key actions(Will be applied later)
     if key == ord('q'):
         running = False
     if key == ord('w'):
-        print("w")
+        print("Stop")
     if key == ord('e'):
-        print("e")
+        print("Resume")
     if key == ord('r'):
-        print("r")
+        print("Restart")
 
 # Release the video capture
 cap.release()
